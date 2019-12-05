1ba7ba4c4f393cac326614add8299053131cfc57
J-Navajo <jnnfr.stnly@gmail.com>
2019-12-05 15:27:33 -0700

    I got all my prompts working

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,45 @@
-<script>
-    let nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    let lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    let symbols = ["!", "@", "#", "$", "%", "&", "*", "(", ")", "-", "?", ".", "+"]
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function generate(){
+//     prompt("How many characters would you like in your password?")
     
-</script>
+    
+//     prompt("Click OK to confirm special characters.")
+//     confirm
+
+//     prompt("Click OK to confirm adding numbers.")
+//     confirm()
+
+//     prompt("Click OK to include lowercase letters.")
+//     confirm()
+
+//     prompt("Click OK to confirm UPPERCASE letters.")
+//     confirm()
+//     let length =  ;
+//     let nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//     let uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//     let lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//     let symbols = ["!", "@", "#", "$", "%", "&", "*", "(", ")", "-", "?", ".", "+"]
+
+// }
+
+// for (generate())
